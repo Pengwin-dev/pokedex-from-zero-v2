67c116690442b9cb3c4213c4e019d02f54e1ff14
Peng.win <Peng.win@DESKTOP-51B3GJ9>
2023-03-30 17:18:20 -0400

    Fixes on Card

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -5,107 +5,56 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import { NavBar } from './UI/NavBar';
+import { v4 as uuidv4 } from 'uuid';
 
 function PokemonCard() {
   const pokeApiDomain = `https://pokeapi.co/api/v2/pokemon/`;
-  const [currentId, setCurrentId] = useState(1);
-  const [pokemon, setPokemon] = useState({sprites:{other:{"official-artwork":{}}}, weight:0, abilities:[]});
-  const [isLoading, setIsLoading] = useState(false);
-  const [weaknes, setWeaknesses] = useState([]);
-  const [statsInfo, setStatsInfo] = useState([]);
 
+  const [currentId, setCurrentId] = useState(1);
+  const [pokemon, setPokemon] = useState({ sprites: {}, weight: 0, abilities: [] });
+  const [loading, setLoading] = useState(false);
+  const [weaknesses, setWeaknesses] = useState([]);
+
+  useEffect(() => {
+    fetch(`${pokeApiDomain}${currentId}`)
+    .then(response => response.json())
+    .then((data)=>
+    {
+      setCurrentId(data.id);
+      setPokemon(data);
+
+      const typeUrls = data.types.map(type => type.type.url);
+      typeUrls.map((url) => fetch(url)
+      .then((r)=>r.json())
+      .then((res)=> setWeaknesses(res.damage_relations.double_damage_from)));
+      setLoading(false)
+    }
+    )
+    .catch((err)=>
+    console.log(err))
+  }, [currentId]);
 
   const getPokemon = (id) => {
     id= id > 150 ? 1 : id < 1 ? 150 : id;
     setCurrentId(id);
   };
 
-  async function setPokemonDamage1 (info) {
-    setWeaknesses([]);
-    let typeUrls= info.types.map(item =>item.type.url);
-    let results = [];
-    
-    await Promise.all(typeUrls.map(async (typeUrl) => {
-      await fetch(typeUrl)
-      .then(response => response.json())
-      .then(data => {
-        let damageFromDouble = data.damage_relations.double_damage_from
-                              .map(x=>x.name);
-        results.push(damageFromDouble);
-      })
-    }));
-
-    results.forEach(element => {
-      element.forEach(newType => {
-        weaknes.push(newType);
-      });
-    });
-
-    setWeaknesses([...new Set(weaknes)]);
-    
-    return weaknes;
-  }
-
-  const getPokemonDamage = (info) => {
-    return setPokemonDamage1(info);
-  }
-  
-  const setStatsInformation = (pokemonInfo1)=>{
-    let label1 = [];
-    let stats1 = [];
-
-    console.log(pokemonInfo1);
-    pokemonInfo1.stats.forEach(element => {
-      label1.push(element.stat.name);
-      stats1.push(element.base_stat);
-    });
-    
-    setStatsInfo({labels: label1, stats: stats1});
-  }
-
-  useEffect(() => {
-    setIsLoading(true);
-    getPokemon(currentId);
-    fetch(`${pokeApiDomain}${currentId}`)
-      .then(response => response.json())
-      .then(pokemonData => {
-        setCurrentId(pokemonData.id);
-        setPokemon(pokemonData);
-        getPokemonDamage(pokemonData);
-        setStatsInformation(pokemonData);
-        setIsLoading(false);
-      })
-      .catch(err => console.error(err));
-  }, [currentId]);
-
   return (
+    
     <div className="App">
+      <NavBar>Pokedex</NavBar>
       <header className="App-header">
-      <NavBar>POKEMONCARD </NavBar>
-        {
-          isLoading ? (
-            <>
-              <h1>Loading.....</h1>
-            </>
-          ) : (
-          <div>
-             
-            <div style=
-              {{
-                borderRadius:'5px',
-                boxShadow: '0 0 0 .4em yellow',
-                display: 'flex',
-                flexDirection:'row',
-                zIndex:100
-
-              }}>
-              <div style= {{marginRight:'-23px', zIndex:1, display:'flex', alignItems:'center'}}>
-                <button onClick = {()=> getPokemon(currentId-1)}>{'<'}</button>
-              </div>
-              <div>
-              <div className='Pokedex-Frame'>
+        <div>
+          <label className="App-Name">Pokedex</label>
+        </div>
+      </header>
+      
+        {loading ? (
+          <></>
+        ) : (
+          <Card className='Pokedex-Frame'>
             <div>
-              <label className="Pokemon-Name">{pokemon.name.toUpperCase()}</label>
+              <label className="Pokemon-Name">{pokemon.name}</label>
             </div>
             <div>
               <img src={pokemon.sprites.front_default} className="Pokemon-Sprite" alt="logo" />
@@ -126,25 +75,33 @@ function PokemonCard() {
             </div>
 
             <div>
-              <label className="Pokemon-Moves">Moves:</label>
+              <label className="Pokemon-Moves">abilities:</label>
               {
                 
                 // select just first 4 moves
-                  pokemon.moves.slice(0,4).map(item => (
+                  pokemon.abilities.map(item => (
                     <div key={uuidv4()}>
-                      <label >{item.move.name}</label>
+                      <label >{item.ability.name}</label>
                     </div>
                 
-              </div>
-              <div style={{marginLeft:'-23px', zIndex:1, display:'flex', alignItems:'center'}}>
-                <button onClick={()=> getPokemon(currentId + 1)}>{'>'}</button>
-              </div>        
-            </div> 
-          </div>
-          )
+                ))
+              }
+            </div>
+
+            <div>
+            <label className="Pokemon-Weakness">Weaknesses:</label>
+                {weaknesses.map(weakness => (
+                  <div key={uuidv4()}>
+                      <label>{weakness.name}</label>
+                  </div>
+                  
+                ))}
+            </div>
+
+          </Card>)
         }
-      </header >
-    </div >
+      
+    </div>
   );
 }
 
